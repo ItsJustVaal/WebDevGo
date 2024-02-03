45d64e5b5ca0798f3308dca7f18248f5ed370549
Robert Marrett <Robmmar90@gmail.com>
2024-02-03 12:21:09 -0700

    added named templates, nav and tailwind

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
-	"path/filepath"
 
 	"github.com/ItsJustVaal/WebDevGo/controllers"
+	"github.com/ItsJustVaal/WebDevGo/templates"
 	"github.com/ItsJustVaal/WebDevGo/views"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -20,9 +20,9 @@ func main() {
 
 	
 	// Gets
-	mainRouter.Get("/", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))))
-	mainRouter.Get("/contact", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))))
-	mainRouter.Get("/faq", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))))
+	mainRouter.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
+	mainRouter.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
+	mainRouter.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
 
 
--- views/template.go
+++ views/template.go
@@ -3,6 +3,7 @@ package views
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
 )
@@ -19,6 +20,16 @@ func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 		http.Error(w, "Invalid data passed to execute", http.StatusInternalServerError)
 		return
 	}
+}
+
+func ParseFS(fs fs.FS, patterns ...string) (Template, error){
+	tpl, err := template.ParseFS(fs, patterns...)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template error: %w", err)
+	}
+	return Template{
+		htmlTpl: tpl,
+	}, nil
 }
 
 func Parse(fp string) (Template, error) {
